e9ef4ed92029b9bf4b2ab3b8b0540dc570e6cb7f
Joakim Gebart <joakim.gebart@eistec.se>
2015-02-13 14:39:41 +0100

    python: Changes for Python3 support.

--- python/byteport/socksipyhandler.py
+++ python/byteport/socksipyhandler.py
@@ -7,14 +7,14 @@ author: e<e@tr0ll.in>
 This module provides a Handler which you can use with urllib2 to allow it to tunnel your connection through a socks.sockssocket socket, with out monkey patching the original socket...
 """
 
-import urllib2
-import httplib
-import socks
+import urllib.request, urllib.error, urllib.parse
+import http.client
+from . import socks
 
-class SocksiPyConnection(httplib.HTTPConnection):
+class SocksiPyConnection(http.client.HTTPConnection):
     def __init__(self, proxytype, proxyaddr, proxyport = None, rdns = True, username = None, password = None, *args, **kwargs):
         self.proxyargs = (proxytype, proxyaddr, proxyport, rdns, username, password)
-        httplib.HTTPConnection.__init__(self, *args, **kwargs)
+        http.client.HTTPConnection.__init__(self, *args, **kwargs)
 
     def connect(self):
         self.sock = socks.socksocket()
@@ -23,11 +23,11 @@ class SocksiPyConnection(httplib.HTTPConnection):
             self.sock.settimeout(self.timeout)
         self.sock.connect((self.host, self.port))
 
-class SocksiPyHandler(urllib2.HTTPHandler):
+class SocksiPyHandler(urllib.request.HTTPHandler):
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kw = kwargs
-        urllib2.HTTPHandler.__init__(self)
+        urllib.request.HTTPHandler.__init__(self)
 
     def http_open(self, req):
         req.add_header('User-Agent', 'Mozilla/5.0')
@@ -37,5 +37,5 @@ class SocksiPyHandler(urllib2.HTTPHandler):
         return self.do_open(build, req)
 
 if __name__ == "__main__":
-    opener = urllib2.build_opener(SocksiPyHandler(socks.PROXY_TYPE_SOCKS4, 'localhost', 9999))
-    print opener.open('http://www.whatismyip.com/automation/n09230945.asp').read()
+    opener = urllib.request.build_opener(SocksiPyHandler(socks.PROXY_TYPE_SOCKS4, 'localhost', 9999))
+    print(opener.open('http://www.whatismyip.com/automation/n09230945.asp').read())
