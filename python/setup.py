64dd09f41a83552c3fb30d6960c4506548392efa
Hans Eklund <hans.eklund@igw.se>
2015-07-10 09:30:47 +0200

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ __author__ = 'hans'
 from distutils.core import setup
 
 setup(name='Byteport API',
-      version='0.51',
+      version='0.52',
       description='Python Clients for Byteport (www.byteport.se)',
       author='Byteport developers',
       author_email='contact@byteport.se',
