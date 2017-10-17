aba3e2e608dac1bf67c6d50d3d2129b74ab3892b
hans <hans.eklund@igw.se>
2017-10-17 14:54:45 +0200

    setup.py fixes.

--- python/setup.py
+++ python/setup.py
@@ -10,4 +10,9 @@ setup(name='Byteport API',
       author_email='contact@byteport.se',
       url='https://github.com/iGW/byteport-api',
       packages=['byteport'],
+      install_requires=[
+            'pytz>=2015.7',
+            'stompest==2.1.6',
+            'paho-mqtt==1.1'
+      ]
 )
