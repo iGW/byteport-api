28b801a45e7068c42a61ee585bc85b25274dfb67
Hanserfaust <hans.eklund@igw.se>
2021-03-01 20:37:47 +0100

    Locked pytz version.

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ setup(name='Byteport API',
       url='https://github.com/iGW/byteport-api',
       packages=['byteport'],
       install_requires=[
-            'pytz>=2015.7',
+            'pytz==2018.9',
             'stompest==2.3.0',
             'paho-mqtt==1.1'
       ]
